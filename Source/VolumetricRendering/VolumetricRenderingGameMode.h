726bf60efcc6affb8623b7829f0e28a36e62bb98
Nilay <nilay963@yahoo.com>
2017-05-22 14:25:55 +0200

    Empty project with starter content

---
+++ Source/VolumetricRendering/VolumetricRenderingGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VolumetricRenderingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOLUMETRICRENDERING_API AVolumetricRenderingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
